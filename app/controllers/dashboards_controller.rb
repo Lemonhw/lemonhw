1f959e4b5a3991397fd086996b80765bb409078f
Dorian Calin <dorin.m.calin@gmail.com>
2023-06-06 12:05:48 +0100

    dashboard show page

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,9 @@
 class DashboardsController < ApplicationController
+  def show
+    # Fetch user-specific data or perform any necessary logic here
+    @user = current_user
+    @plans = @user.weekly_plans
+
+    # Render the dashboard view
+  end
 end
