9c7abab7a7c95706e636d0a383627ad3ad9d21cb
Pouyan Soltanpour <soltanpour.p@gmail.com>
2023-06-07 12:07:29 +0100

    seed revised

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # db/seeds.rb
-
+p "start seeding...."
 10.times do |i|
   # Create a new user
   user = User.create!(
@@ -24,7 +24,7 @@
   weekly_plan = WeeklyPlan.create!(
     fitness_goal: "Weight Loss",
     current_weight: 85 + i,
-    user: user,
+    user_id: user.id,
     weight_goal: 75 + i
   )
 
@@ -32,32 +32,21 @@
   (1..7).each do |day_number|
     day_plan = DayPlan.create!(
       day_number: day_number,
-      weekly_plan: weekly_plan
+      weekly_plan_id: weekly_plan.id
     )
 
     # Create a diet plan for each day
     DietPlan.create!(
-      day_plan: day_plan,
-      day_plan_content:
-        {
-          breakfast: "Oatmeal with fruits",
-          lunch: "Grilled chicken salad",
-          snack: "Almonds and a banana",
-          dinner: "Quinoa and grilled vegetables"
-        }.to_json
+      day_plan_id: day_plan.id,
+      day_plan_content: "breakfast: Oatmeal with fruits, lunch: Grilled chicken salad, snack: Almonds and a banana, dinner: Quinoa and grilled vegetables"
     )
 
     # Create an exercise plan for each day
     ExercisePlan.create!(
-      day_plan: day_plan,
-      day_plan_content:
-        {
-          exercise_1: "Treadmill - 30 minutes",
-          exercise_2: "Squats - 3 sets of 10",
-          exercise_3: "Bench Press - 3 sets of 10",
-          exercise_4: "Lunges - 3 sets of 10",
-          exercise_5: "Plank - 3 sets of 1 minute"
-        }.to_json
+      day_plan_id: day_plan.id,
+      day_plan_content: "exercise_1: Treadmill - 30 minutes, exercise_2: Squats - 3 sets of 10, exercise_3: Bench Press - 3 sets of 10, exercise_4: Lunges - 3 sets of 10, exercise_5: Plank - 3 sets of 1 minute"
     )
   end
 end
+
+p "seeding done!"
