7ffdc4566b447b58a805e53796f5c1072bb52a27
Stefan Shambrook <stefshambrook@gmail.com>
2023-06-12 15:54:53 +0100

    Create content for diet plan view

--- app/controllers/day_plans_controller.rb
+++ app/controllers/day_plans_controller.rb
@@ -2,7 +2,7 @@ class DayPlansController < ApplicationController
   def show
     @day_plan = DayPlan.find(params[:id])
     @diet_plan = @day_plan.diet_plan
-    @diet_plan_content = @diet_plan.day_plan_content
+    @meal = @diet_plan.day_plan_content
     # @exercise_plan = @day_plan.exercise_plan
     # @exercise_plan_content = @exercise_plan.day_plan_content
 
