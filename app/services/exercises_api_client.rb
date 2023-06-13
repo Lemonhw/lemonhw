2ba7517865f57ea55170b61f6950971fe090c79a
Stefan Shambrook <stefshambrook@gmail.com>
2023-06-13 10:49:57 +0100

    Create seed for exercises table

---
+++ app/services/exercises_api_client.rb
@@ -0,0 +1,15 @@
+class ExercisesApiClient
+  def fetch_exercises(type, muscle, difficulty, offset)
+    url = URI("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=#{type}&muscle=#{muscle}&difficulty=#{difficulty}&offset=#{offset}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = 'bbe3ae6043msh65faa6d70c21d77p153ce9jsne9638654940e'
+    request["X-RapidAPI-Host"] = 'exercises-by-api-ninjas.p.rapidapi.com'
+
+    response = http.request(request)
+    response.read_body
+  end
+end
