754610eac840c5ae066bf735f27069d73fb0f71f
Dorian Calin <dorin.m.calin@gmail.com>
2023-06-13 17:10:51 +0100

    added bmi progress bar

--- db/schema.rb
+++ db/schema.rb
@@ -92,13 +92,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_135124) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.integer "age"
     t.integer "height"
     t.string "dietary_requirements", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "surname"
-    t.date "date_of_birth"
-    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
