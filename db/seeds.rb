a7349af55d83c7019b19e5a1659d573e2959f660
Dorian Calin <dorin.m.calin@gmail.com>
2023-06-13 15:05:27 +0100

    added the results represented by graphs on the overview

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # db/seeds.rb
 p "start seeding...."
-# 10.times do |i|
+10.times do |i|
   # Create a new user
   # user = User.create!(
   #   email: "user#{i}@example.com",
@@ -21,53 +21,54 @@ p "start seeding...."
   # )
 
   # Create a new weekly plan for the user
-  # weekly_plan = WeeklyPlan.create!(
-  #   fitness_goal: "Weight Loss",
-  #   current_weight: 85 + i,
-  #   # user_id: user.id,
-  #   weight_goal: 75 + i
-  # )
+  weekly_plan = WeeklyPlan.create!(
+    fitness_goal: "Weight Loss",
+    current_weight: 85 + i,
+    # user_id: user.id,
+    profile: Profile.last,
+    weight_goal: 75 + i
+  )
 
   # Iterate over 7 days to create day plans
-  # (1..7).each do |day_number|
-  #   day_plan = DayPlan.create!(
-  #     day_number: day_number,
-  #     weekly_plan_id: weekly_plan.id
-  #   )
+  (1..7).each do |day_number|
+    day_plan = DayPlan.create!(
+      day_number: day_number,
+      weekly_plan_id: weekly_plan.id
+    )
 
     # Create a diet plan for each day
-    # DietPlan.create!(
-    #   day_plan_id: day_plan.id,
-    #   day_plan_content: "breakfast: Oatmeal with fruits, lunch: Grilled chicken salad, snack: Almonds and a banana, dinner: Quinoa and grilled vegetables"
-    # )
+    DietPlan.create!(
+      day_plan_id: day_plan.id,
+      day_plan_content: "breakfast: Oatmeal with fruits, lunch: Grilled chicken salad, snack: Almonds and a banana, dinner: Quinoa and grilled vegetables"
+    )
 
     # Create an exercise plan for each day
-    # ExercisePlan.create!(
-    #   day_plan_id: day_plan.id,
-    #   day_plan_content: "exercise_1: Treadmill - 30 minutes, exercise_2: Squats - 3 sets of 10, exercise_3: Bench Press - 3 sets of 10, exercise_4: Lunges - 3 sets of 10, exercise_5: Plank - 3 sets of 1 minute"
-    # )
-  # end
+    ExercisePlan.create!(
+      day_plan_id: day_plan.id,
+      day_plan_content: "exercise_1: Treadmill - 30 minutes, exercise_2: Squats - 3 sets of 10, exercise_3: Bench Press - 3 sets of 10, exercise_4: Lunges - 3 sets of 10, exercise_5: Plank - 3 sets of 1 minute"
+    )
+  end
+end
 
-  Video.destroy_all
+Video.destroy_all
 
-  Video.create(
-    title: "Video 1",
-    url: "https://www.youtube.com/embed/VI3muCQbMyo"
-  )
+Video.create(
+  title: "Video 1",
+  url: "https://www.youtube.com/embed/VI3muCQbMyo"
+)
 
-  Video.create(
-    title: "Video 2",
-    url: "https://www.youtube.com/embed/S12mLBAECV0"
-  )
-  Video.create(
-    title: "Video 3",
-    url: "https://www.youtube.com/embed/eNEgR1fyros"
-  )
+Video.create(
+  title: "Video 2",
+  url: "https://www.youtube.com/embed/S12mLBAECV0"
+)
+Video.create(
+  title: "Video 3",
+  url: "https://www.youtube.com/embed/eNEgR1fyros"
+)
 
-  Video.create(
-    title: "Video 4",
-    url: "https://www.youtube.com/embed/PZvwzobkCUM"
-  )
-# end
+Video.create(
+  title: "Video 4",
+  url: "https://www.youtube.com/embed/PZvwzobkCUM"
+)
 
 p "seeding done!"
