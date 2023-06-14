9c289b898dd4b5dd1490620e5ccd1aaee080d8b3
Stefan Shambrook <stefshambrook@gmail.com>
2023-06-14 09:23:29 +0100

    Minor tweaks

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -16,10 +16,10 @@ class DashboardsController < ApplicationController
     end
   end
 
-
   def exercise_plan
     @profile = current_user.profile
     @weekly_plans = @profile.weekly_plans.order(created_at: :desc)
+    @exercise_plan_content = 
     if @weekly_plans.any?
       @weekly_plan = @weekly_plans.first
       @day_plans = @weekly_plan.day_plans.order(created_at: :asc)
